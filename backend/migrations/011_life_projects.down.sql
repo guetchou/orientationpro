4281b29607af5019049d979aaf5d2e09c9c74fa7
guetchou <140221073+guetchou@users.noreply.github.com>
2026-07-29 08:55:39 +0100

    feat(db): ajouter le rollback du projet de vie

---
+++ backend/migrations/011_life_projects.down.sql
@@ -0,0 +1,7 @@
+DROP TABLE IF EXISTS life_project_events;
+DROP TABLE IF EXISTS life_project_action_items;
+DROP TABLE IF EXISTS life_project_action_plans;
+DROP TABLE IF EXISTS life_project_criteria;
+DROP TABLE IF EXISTS life_project_active_scenarios;
+DROP TABLE IF EXISTS life_project_scenarios;
+DROP TABLE IF EXISTS life_projects;
