fcbf0b27b1e491d303b248d39509ff5f044ede63
guetchou <140221073+guetchou@users.noreply.github.com>
2026-08-04 01:01:25 +0100

    fix(auth): conserver les insertions de session contrôlées

--- backend/migrations/025_auth_session_timeouts.up.sql
+++ backend/migrations/025_auth_session_timeouts.up.sql
@@ -9,6 +9,8 @@ SET
   absolute_expires_at = LEAST(expires_at, DATE_ADD(created_at, INTERVAL 12 HOUR));
 
 ALTER TABLE auth_sessions
-  MODIFY idle_expires_at DATETIME(3) NOT NULL,
-  MODIFY absolute_expires_at DATETIME(3) NOT NULL,
+  MODIFY idle_expires_at DATETIME(3) NOT NULL
+    DEFAULT (CURRENT_TIMESTAMP(3) + INTERVAL 30 MINUTE),
+  MODIFY absolute_expires_at DATETIME(3) NOT NULL
+    DEFAULT (CURRENT_TIMESTAMP(3) + INTERVAL 12 HOUR),
   ADD KEY idx_auth_sessions_timeouts (account_id, revoked_at, idle_expires_at, absolute_expires_at);
